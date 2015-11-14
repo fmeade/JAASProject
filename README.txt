d972e9246259e6d9097a0dc911a41b244c1c5998
Forrest Meade <forrest.meade@mantech.com>
2015-11-14 17:45:02 -0500

    started manipulation

---
+++ README.txt
@@ -0,0 +1,24 @@
+The source files are in the directory: src.
+
+
+To run the program from Command line (in Mac/Linux or Windows command prompt): 
+	(1) Compile the code: 
+		javac src/*.java
+	(2) Run the code: 
+		java -Djava.security.manager \
+			-Djava.security.policy==JAASPolicies.txt \
+			 -Djava.security.auth.login.config==JAASPolicy.txt \
+			  src/JAASSystemDriver 
+
+To run the program on Eclipse:
+ 	(1) Create a new Java project in Eclipse. 
+	(2) Select the directory JAASMaster to import the source files into
+		Eclipse.
+	(3) Select the JAASSystemDriver.java file. 
+	(4) In the menu option: Run->Run Configurations, select the tab 
+		"Arguments"
+	(5) In the VM arguments add: 
+ 		-Djava.security.manager -Djava.security.policy==JAASPolicies.txt -Djava.security.auth.login.config==JAASPolicy.txt  
+	(6) Run. 
+
+Usernames and passwords that will work: root/security or team/security. 
---
+++ src/JAASSystemDriver.java
@@ -0,0 +1,86 @@
+package src;
+import java.io.File;
+import java.io.FilePermission;
+import java.security.PrivilegedAction;
+import java.util.Iterator;
+import java.util.Set;
+import java.security.Principal;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.*;
+
+import src.RootAction; 
+
+
+/* 
+ * This class demonstrates 
+ *        how to create a logincontext;
+ *        how to invoke the login method; 
+ *        how to store and extract the logged in subject.
+ * This class is also the driver program. 
+ * This class uses: 
+ *       class LoginModuleExample: implements login method 
+ *                                 to perform authentication, 
+ *                                 creates a Subject object, and
+ *                                 implements the logout method.                     
+ */
+
+public class JAASSystemDriver {
+	
+		
+    static LoginContext lc; 	
+	
+    public static void main(String[] args) {
+		
+		/* Create a call back handler. This call back handler will be populated with
+                 * different callbacks by the various login modules. For example, 
+                 * if a login module implements a username/password style login, it populates this object
+                 * with NameCallback (to get username) and PasswordCallback (which gets password).
+		 */
+		JAASCallBackHandler cbe = new JAASCallBackHandler(); 
+		
+		/* Create a new login context. 
+		 * @param Policy Name : We defined a policy in the file JAASPolicy.txt 
+		 *                      and it is called "JAASExample"
+		 * @param Call Back Handler
+		 */
+		try {
+			lc = new LoginContext("JAASMaster", cbe);
+		}
+		catch (LoginException e) {
+			System.err.println("Login exception."); 
+		}
+		
+		/* 
+		 * Perform login.
+		 */
+		try {
+			lc.login(); 
+			
+			// If we reach this point then login has succeeded.
+			System.out.println("You are now logged in!");
+			/* 
+			 * Print the various Principals attached with the logged Subject.
+			 * In this example, we attach only one principal with each subject.
+			 */
+			Subject loggedUser = lc.getSubject();
+			Set principals = lc.getSubject().getPrincipals();
+			Iterator i = principals.iterator();
+			while (i.hasNext()) {
+				String s = ((Principal)i.next()).getName();
+			}
+		}
+				
+		catch (LoginException e) {
+			System.out.println("Username/password incorrect! " + e);
+			
+		}
+		catch (SecurityException e) {
+			System.out.println(" " + e);
+		}
+			
+		
+	}
+	
+	
+}
